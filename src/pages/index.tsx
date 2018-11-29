8136496fd6941109dae718e1b2d1ec34d644d123
Olivia <olivia_github@outlook.com>
2018-11-29 19:32:54 +0000

    Improving post page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,7 +50,7 @@ export const indexPageQuery = graphql`
                   ...GatsbyImageSharpSizes
                 }
               }
-          }
+            }
           }
         }
       }
--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,30 +1,50 @@
 import * as React from 'react'
 import * as styles from './blog-post.module.scss'
 
+import Img from 'gatsby-image'
 import Page from '../components/Page';
 import { graphql } from 'gatsby'
 
 interface BlogPostTemplateProps {
-    data: {
-        site: Site
-        markdownRemark: MarkdownRemark
-    }
+  data: {
+    site: Site
+    markdownRemark: MarkdownRemark
+  }
 }
 
 class BlogPostTemplate extends React.Component<BlogPostTemplateProps> {
-    render() {
-        const post = this.props.data.markdownRemark;
+  render() {
+    const post = this.props.data.markdownRemark;
 
-        return (
-          <Page>
+    return (
+      <Page>
 
-            <div>
+        <div className={styles.contentContainer}>
+          <div className={styles.postContainer}>
+
+            {/* Featured image */}
+            <div className={styles.postImage}>
+              <Img sizes={post.frontmatter.featuredImage.childImageSharp.sizes} />
+            </div>
+
+            {/* Title */}
+            <div className={styles.postTitle}>
+              <h1>{post.frontmatter.title}</h1>
+            </div>
+
+            {/* Content */}
+            <div className={styles.postContent}>
               <div dangerouslySetInnerHTML={{ __html: post.html }} />
             </div>
 
-          </Page>
-        );
-    }
+          </div>
+
+          <div className={styles.sidebar} />
+        </div>
+
+      </Page>
+    );
+  }
 }
 export default BlogPostTemplate
 
@@ -36,6 +56,13 @@ export const pageQuery = graphql`
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
+        featuredImage {
+          childImageSharp{
+            sizes(maxWidth: 700, maxHeight: 300) {
+              ...GatsbyImageSharpSizes
+            }
+          }
+        }
       }
     }
   }
