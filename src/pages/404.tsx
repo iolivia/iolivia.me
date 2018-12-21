54ebb0d4396e77bd0adc3fdfeb6ea79c815f1ee7
Olivia <olivia_github@outlook.com>
2018-12-21 20:06:18 +0200

    Changing fonts

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,7 @@ import Page from '../components/Page';
 
 export default class IndexPage extends React.Component<{}, {}> {
 
-  private quotes : string[] = [
+  private quotes: string[] = [
     "I think you travel to search and you come back home to find yourself there.",
     "I am a pisces, a fish out of water, searching for a way back home.",
     "If you want to find the real competition, just look in the mirror. After awhile you'll see your rivals scrambling for second place.",
@@ -18,23 +18,23 @@ export default class IndexPage extends React.Component<{}, {}> {
 
       <Page>
 
-          {/* Heading */}
-          <p className={styles.huge}>
-            404 - Not found
+        {/* Heading */}
+        <p className={styles.huge}>
+          404 - Not found
           </p>
 
-          <h1>
-            What you're looking for cannot be found. Search inside your soul.
+        <h1>
+          What you're looking for cannot be found. Search inside your soul.
           </h1>
 
-          {/* Quotes */}
-          <div className={styles.quotes}>
-            {this.buildQuotes()}
-          </div>
+        {/* Quotes */}
+        <div className={styles.quotes}>
+          {this.buildQuotes()}
+        </div>
 
-          <p>
-            The standard Lorem Ipsum passage, used since the 1500s
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        <p>
+          The standard Lorem Ipsum passage, used since the 1500s
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
           </p>
 
       </Page>
@@ -44,7 +44,7 @@ export default class IndexPage extends React.Component<{}, {}> {
   private buildQuotes = () => {
     const quoteElements = [];
 
-    for(const quote of this.quotes) {
+    for (const quote of this.quotes) {
       quoteElements.push((
         <h3><p>{quote}</p></h3>
       ));
