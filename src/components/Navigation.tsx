fe1b3ed502cd9fc1fad08db6760b5f3e1e77dd72
Olivia <olivia_github@outlook.com>
2018-12-22 17:37:01 +0200

    Hide navbar on phones

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,40 +2,44 @@ import * as React from 'react'
 import styled from "styled-components"
 
 const NavBar = styled.nav`
-  display: block;
-  width: 100%;
-  height: 6.5rem;
-  background: #fff;
-  z-index: 99;
-  border-top: 1px solid #eee;
-  border-bottom: 1px solid #eee;
+display: block;
+width: 100%;
+height: 6.5rem;
+background: #fff;
+z-index: 99;
+border-top: 1px solid #eee;
+border-bottom: 1px solid #eee;
 `;
 
 const NavBarLink = styled.a`
-  text-transform: uppercase;
-  font-size: 11px;
-  font-weight: 600;
-  letter-spacing: .2rem;
-  text-decoration: none;
-  line-height: 6.5rem;
-  color: #222;
+text-transform: uppercase;
+font-size: 11px;
+font-weight: 600;
+letter-spacing: .2rem;
+text-decoration: none;
+line-height: 6.5rem;
+color: #222;
 
-  :hover {
-    color: #f7484e; 
-  }
+:hover {
+  color: #f7484e; 
+}
 `;
 
 const NavBarItem = styled.li`
-  position: relative;
-  margin-left: 35px;
-  float: left;
-  margin-bottom: 0; 
+position: relative;
+margin-left: 35px;
+float: left;
+margin-bottom: 0; 
+
+@media (max-width: 550px) {
+  display: none;
+}
 `
 
 const NavBarItems = styled.ul`
-  list-style: none;
-  margin-bottom: 0; 
-  float: right;
+list-style: none;
+margin-bottom: 0; 
+float: right;
 `
 
 export default class Navigation extends React.Component<{}, {}> {
