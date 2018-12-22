88b5d9339d4b326d42775576a2d12e9b97b83b95
Olivia <olivia_github@outlook.com>
2018-12-22 17:15:04 +0200

    Add favicons

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -2,6 +2,12 @@ import * as React from 'react'
 
 import Helmet from 'react-helmet'
 
+import favicon16 from "../../static/favicon-16x16.png"
+import favicon32 from "../../static/favicon-32x32.png"
+import faviconApple from "../../static/apple-touch-icon.png"
+import faviconChrome192 from "../../static/android-chrome-192x192.png"
+import faviconChrome512 from "../../static/android-chrome-512x512.png"
+
 interface SEOProps {
     description: string;
     title: string;
@@ -12,7 +18,7 @@ export default class SEO extends React.Component<SEOProps, {}> {
     public render() {
 
         const { description, title } = this.props;
-        
+
         return (
             <div>
 
@@ -21,6 +27,14 @@ export default class SEO extends React.Component<SEOProps, {}> {
                     htmlAttributes={{ lang: 'en' }}
                     meta={[{ name: 'description', content: description }]}
                     title={title}
+
+                    link={[
+                        { rel: 'icon', type: 'image/png', sizes: "16x16", href: `${favicon16}` },
+                        { rel: 'icon', type: 'image/png', sizes: "32x32", href: `${favicon32}` },
+                        { rel: 'icon', type: 'image/png', sizes: "192x192", href: `${faviconChrome192}` },
+                        { rel: 'icon', type: 'image/png', sizes: "512x512", href: `${faviconChrome512}` },
+                        { rel: 'apple-touch-icon', type: 'image/png', sizes: "180x180", href: `${faviconApple}` },
+                    ]}
                 />
 
             </div>
